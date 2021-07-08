9eba21bf97f98cff51df20251e151e5f80454fbb
Jerry Plotke <jerry.plotke@gmail.com>
2021-07-08 14:24:15 -0500

    foreign keys

--- models/index.js
+++ models/index.js
@@ -5,16 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+        foreignKey: 'category_id'
+    }),
 
-// Categories have many Products
+    // Categories have many Products
+    Category.hasMany(Product, {
+        foreignKey: 'category_id'
+    }),
 
-// Products belongToMany Tags (through ProductTag)
+    // Products belongToMany Tags (through ProductTag)
+    Product.belongsToMany(Tag, {
+        through: ProductTag,
+        foreignKey: 'product_tag'
+    }),
 
-// Tags belongToMany Products (through ProductTag)
+    // Tags belongToMany Products (through ProductTag)
+    Tag.belongsToMany(Product, {
+        through: ProductTag,
+        foreignKey: 'tag_id'
+    }),
 
-module.exports = {
-    Product,
-    Category,
-    Tag,
-    ProductTag,
-};
+    module.exports = {
+        Product,
+        Category,
+        Tag,
+        ProductTag,
+    };
